1ab6f22c275a20bc2a05d64b352c051ac31c6089
Akash <111729806+Akash799@users.noreply.github.com>
2025-02-24 18:19:08 +0530

    Update startcontainer.sh

--- script/startcontainer.sh
+++ script/startcontainer.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull akashak799/simple-python-app:latest
+sudo docker pull akashak799/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d --name akash799 -p 5000:5000 akashak799/simple-python-app:latest
+sudo docker run -d --name akash799 -p 5000:5000 akashak799/simple-python-app:latest
