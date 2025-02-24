216af1fcb99a219a8af5baa1900a6d785b0e5fe1
Akash799 <www.akashak@gmail.com>
2025-02-25 02:15:22 +0530

    edited script

--- scripts/stopcont.sh
+++ scripts/stopcont.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-set -e
+set -e  # Exit if any command fails
 
-# Stop the running container (if any)
-#!/bin/bash
+echo "Finding the running container..."
 
-containerID=$(docker ps | awk 'NR==2 {print $1}')
+# Get the first running container ID (if any)
+containerID=$(docker ps -q | head -n 1)
 
-
-docker rm -f $containerID
+if [ -n "$containerID" ]; then
+    echo "Stopping and removing container: $containerID"
+    docker stop "$containerID"
+    docker rm -f "$containerID"
+else
+    echo "No running containers found. Skipping removal."
+fi
